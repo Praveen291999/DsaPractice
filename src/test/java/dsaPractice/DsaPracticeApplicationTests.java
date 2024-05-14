21c70c3a9e139484c26ad5a7fcb0ea6ef7c597b7
Praveen Kumar R <r-praveenkumar@DISNEYOMC.COM>
2024-05-14 16:45:28 +0530

    Initial Project commit

---
+++ src/test/java/dsaPractice/DsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dsaPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
